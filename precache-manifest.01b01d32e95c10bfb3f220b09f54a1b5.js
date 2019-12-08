b20d21ffa10e4cf063afbb3b1d0d5aab8ef050ae
Alexsavchuk95 <savchukalex57@gmail.com>
2019-12-08 15:59:15 +0300

    Updates

---
+++ precache-manifest.01b01d32e95c10bfb3f220b09f54a1b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c2f0b82a1f619e25f64470a89da7c3",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "be4205c77ade94e3a372",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.7f7aadda.chunk.css"
+  },
+  {
+    "revision": "5ecc086812e84649482c",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.f0c2c08e.chunk.js"
+  },
+  {
+    "revision": "be4205c77ade94e3a372",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.65ed008f.chunk.js"
+  },
+  {
+    "revision": "26f980cacd962de52565",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.9d9945f4.js"
+  }
+]);
